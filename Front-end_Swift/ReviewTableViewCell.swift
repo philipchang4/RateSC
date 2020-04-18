7491a010390317af72f67c1cc76cd07ef34975a2
Tyler Jensen <tylerjen@usc.edu>
2020-04-18 17:23:12 -0400

    Add files via upload

---
+++ Front-end_Swift/ReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewTableViewCell.swift
+//  RateSC2.0
+//
+//  Created by Susan Jensen on 4/18/20.
+//  Copyright © 2020 USC. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Front-end_Swift/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RateSC2.0
+//
+//  Created by Susan Jensen on 4/8/20.
+//  Copyright © 2020 USC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
