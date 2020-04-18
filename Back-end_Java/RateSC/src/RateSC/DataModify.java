640370f8f62eac8f22ed3d6831b7d9b0f690795c
Ekua Dennis <ebdennis@usc.edu>
2020-04-18 15:37:20 -0700

    Update DataModify.java

--- Back-end_Java/RateSC/src/RateSC/DataModify.java
+++ Back-end_Java/RateSC/src/RateSC/DataModify.java
@@ -1,6 +1,8 @@
-package RateSC;
+package RateSC.RateSCAPI;
+
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,7 +13,7 @@ import java.util.Calendar;
 public class DataModify {
 	
 	private static DataModify dm = new DataModify();
-	ArrayList<Rating> allRatings = new ArrayList<Rating>();
+	ArrayList<RatedObject> allObjects = new ArrayList<RatedObject>();
 	
 	
 	public static DataModify getData() {
@@ -39,7 +41,27 @@ public class DataModify {
 		return allCategories;
 	}
 	
-	public ArrayList<RatedObject> getRatedObjectbyCategory(String categoryName){
+	public void getRatedObjects() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT* FROM RatedObjects");
+			while(rs.next()) {
+				String name = rs.getString("title");
+				Double avg = rs.getDouble("averageRating");
+				RatedObject ro = new RatedObject(name,avg);
+				allObjects.add(ro);
+			}
+		}
+		catch(SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		}
+	}
+	
+	public ArrayList<RatedObject> getRatedObjectsByCategory(String categoryName){
 		ArrayList<RatedObject> ObjectsbyCategory = new ArrayList<RatedObject>();
 		Connection conn = null;
 		Statement st = null;
@@ -48,12 +70,10 @@ public class DataModify {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			st = conn.createStatement();
 			rs = st.executeQuery("SELECT * FROM RatedObject ro, Category c WHERE  \n" + 
-					"ro.categoryID = c.categoryID");
+					"ro.categoryID = c.categoryID AND c.title = " + categoryName);
 			while(rs.next()) {
 				String title = rs.getString("title");
-				//Double sum = rs.getDouble("sumRatings");
 				Double avg = rs.getDouble("averageRating");
-				//int total = rs.getInt("totalRatings");
 				RatedObject ro = new RatedObject(title, avg);
 				ObjectsbyCategory.add(ro);
 			}
@@ -63,8 +83,10 @@ public class DataModify {
 		}
 		return ObjectsbyCategory;
 	}
-	public ArrayList<RatedObject> getRatedObjectbyName(String categoryName, String searchName){
-		ArrayList<RatedObject> ObjectsbyName = new ArrayList<RatedObject>();
+	
+	
+	public RatedObject getRatedObjectByName(String categoryName, String searchName){
+		
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
@@ -72,30 +94,60 @@ public class DataModify {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			st = conn.createStatement();
 			rs = st.executeQuery("SELECT * FROM RatedObject ro, Category c WHERE\n" + 
-					"ro.categoryID = c.categoryID");
+					"ro.categoryID = c.categoryID AND c.title = " + categoryName + "AND"
+					+ "ro.title =" + searchName);
 			while(rs.next()) {
 				String title = rs.getString("title");
-				//Double sum = rs.getDouble("sumRatings");
 				Double avg = rs.getDouble("averageRating");
-				//int total = rs.getInt("totalRatings");
 				RatedObject ro = new RatedObject(title, avg);
-				ObjectsbyName.add(ro);
+				if(getRatedObject(ro.getName()) != null) {
+					return getRatedObject(ro.getName());
+				}
 			}
 		}
 		catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
 		}
-		return ObjectsbyName;
+		return null;
 	}
 	
-	public void getRatingsbyRatedObject(ArrayList<RatedObject> ObjectsinCategory) {
+	public ArrayList<RatedObject> getRatedObjectbyUser(String username) {
+		ArrayList<RatedObject> ObjectsbyUser = new ArrayList<RatedObject>();
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * FROM Ratings r, RatedObject ro WHERE r.ratedObjectID = ro.ratedObjectID");
+			rs = st.executeQuery("SELECT userID FROM User WHERE User.username =" + username);
+			rs.next();
+			Integer ID = rs.getInt("userID");
+			rs = st.executeQuery("SELECT * from RatedObject WHERE userID=" + ID.toString());
+			while(rs.next()) {
+				String title = rs.getString("title");
+				Double avg = rs.getDouble("averageRating");
+				RatedObject ro = new RatedObject(title, avg);
+				ObjectsbyUser.add(ro);
+			}
+			return ObjectsbyUser;
+		}
+		catch(SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		}
+		return null;
+		
+	}
+	
+	public ArrayList<Rating> getRatingsByRatedObject(String RatedObjectName) {
+		ArrayList<Rating> allRatings = new ArrayList<Rating>();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM Ratings r, RatedObject ro WHERE r.ratedObjectID = ro.ratedObjectID"
+					+ "AND ro.title="+ RatedObjectName);
 			while(rs.next()) {
 				String title = rs.getString("title");
 				String description = rs.getString("rdescription");
@@ -104,21 +156,23 @@ public class DataModify {
 				Double rating = rs.getDouble("rating");
 				boolean privacy = rs.getBoolean("privacy");
 				Double avg = rs.getDouble("averageRating");
-				RatedObject ro = getRatedObject(title, ObjectsinCategory);
+				RatedObject ro = getRatedObject(title);
 				Rating r = new Rating(ro, description, dateCreated, numLikes, rating, privacy);
 				allRatings.add(r);
 			}
-			
+			return allRatings;
 		}
 		catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
 		}
+		return null;
 	}
 	
-	public void getRatingsbyName(String user_name, ArrayList<RatedObject> ObjectsbyName) {
+	public  ArrayList<Rating> getRatingsByUser(String user_name) {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
+		ArrayList<Rating> allRatings = new ArrayList<Rating>();
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			st = conn.createStatement();
@@ -134,27 +188,49 @@ public class DataModify {
 				Double rating = rs.getDouble("rating");
 				boolean privacy = rs.getBoolean("privacy");
 				Double avg = rs.getDouble("averageRating");
-				RatedObject ro = getRatedObject(title, ObjectsbyName);
+				RatedObject ro = getRatedObject(title);
 				Rating r = new Rating(ro, description, dateCreated, numLikes, rating, privacy);
 				allRatings.add(r);
 			}
+			return allRatings;
 			
 		}
 		catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
 		}
+		return null;
 	}
 	
 	
 	
-	public RatedObject getRatedObject(String title, ArrayList<RatedObject> Objects) {
-		for(int i = 0; i < allRatings.size();i++) {
-			if (Objects.get(i).getName() == title) {
-				return Objects.get(i);
+	public RatedObject getRatedObject(String title) {
+		for(int i = 0; i < allObjects.size();i++) {
+			if (allObjects.get(i).getName() == title) {
+				return allObjects.get(i);
 			}
 		}
 		return null;
 	}
+	
+	public Rating getRatingbyID(Integer ID) {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * From Rating WHERE ratingID =" + ID.toString());
+			rs.next();
+			
+		}
+		
+		catch(SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		
+		}
+		return null;
+	}
+	
 	
 	public void addRating(String Category, String description, int num, Double rating,
 			boolean privacy) {
@@ -167,7 +243,7 @@ public class DataModify {
 			Calendar c = Calendar.getInstance();
 			String date = c.getTime().toString();
 			
-			rs = st.executeQuery("INSERT INTO Rating (title, rdescription, dateCreated, numLikes,"
+			int row = st.executeUpdate("INSERT INTO Rating (title, rdescription, dateCreated, numLikes,"
 					+ "numRating, privacy) VALUES ('" + Category + "','" + description + "','" + date +
 					"',0,'" + rating.toString() + "'," + privacy + ");");
 		}
@@ -177,22 +253,25 @@ public class DataModify {
 		}
 	}
 	
-	public void addRatedObject(String name) {
+	public void addRatedObject(String name, String CategoryName) {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/SCRater?user=root&password=gymnast_82&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			st = conn.createStatement();
-			rs = st.executeQuery("INSERT INTO RatedObject (title, sumRatings, averageRating, totalRatings) VALUES ('" + 
-			name + "', 0.0, 0.0, 0);");
+			rs = st.executeQuery("SELECT categoryID FROM Category WHERE title = '" + CategoryName + "'");
+			rs.next();
+			Integer CID= rs.getInt("categoryID");
+			int row = st.executeUpdate("INSERT INTO RatedObject (title, sumRatings, averageRating, totalRatings, categoryID) VALUES ('" + 
+			name + "', 0.0, 0.0, 0, " + CID.toString() + ");");
 		}
 		catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
 		}
 	}
 	
-	public void likeRating(String ro_name) {
+	public void likeRating(Integer rating_ID) {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
@@ -206,15 +285,11 @@ public class DataModify {
 				Integer numRatings = rs.getInt("numLikes");
 			}
 			if (count == 0) {
-				rs = st.executeQuery("UPDATE Likes SET numLikes = 1 WHERE userID = likeID");
+				int row = st.executeUpdate("UPDATE Likes SET numLikes = 1 WHERE userID = likeID");
 			}
-			
-			
-			
 		}
 		catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
-		
 		}
 	}
 }
